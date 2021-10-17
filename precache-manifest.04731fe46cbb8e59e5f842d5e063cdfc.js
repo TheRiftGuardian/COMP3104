80b2363d0d555b3df198cce0b92ffb83000d1f50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-17 06:14:01 +0000

    Deploy TheRiftGuardian/COMP3104 to github.com/TheRiftGuardian/COMP3104.git:gh-pages

---
+++ precache-manifest.04731fe46cbb8e59e5f842d5e063cdfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f60011286d46f525d91",
+    "url": "/TheRiftGuardian/COMP3104/static/css/main.5d4ea9dc.chunk.css"
+  },
+  {
+    "revision": "6f60011286d46f525d91",
+    "url": "/TheRiftGuardian/COMP3104/static/js/main.6f600112.chunk.js"
+  },
+  {
+    "revision": "e8465b85f1b56db70943",
+    "url": "/TheRiftGuardian/COMP3104/static/js/runtime~main.e8465b85.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/TheRiftGuardian/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "f038f7450c63cf3d425d08b48fed5da4",
+    "url": "/TheRiftGuardian/COMP3104/index.html"
+  }
+];
